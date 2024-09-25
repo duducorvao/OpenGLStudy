64ce68fb384f79ecbe2159d6e99e53ec4774986f
Eduardo Lustosa <eduardo.lustosa@kokku.local>
2024-09-25 15:22:44 -0300

    Implemented moving triangle with uniform matrix instead of uniform float value

--- OpenGLStudyCourse/OpenGLStudyCourseMain.cpp
+++ OpenGLStudyCourse/OpenGLStudyCourseMain.cpp
@@ -5,10 +5,14 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader, uniformXMove;
+GLuint VAO, VBO, shader, uniformModel ;
 
 float triOffset = 0.0f;
 float triMaxOffset = 0.7f;
@@ -20,11 +24,11 @@ static const char* vShader = "                                        \n\
                                                                       \n\
 layout (location = 0) in vec3 pos;                                    \n\
                                                                       \n\
-uniform float xMove;                                                  \n\
+uniform mat4 model;                                                   \n\
                                                                       \n\
 void main()                                                           \n\
 {                                                                     \n\
-    gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0); \n\
+    gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0); \n\
 }";
 
 // Fragment Shader
@@ -128,7 +132,7 @@ void CompileShaders()
         return;
     }
 
-    uniformXMove = glGetUniformLocation(shader, "xMove");
+    uniformModel = glGetUniformLocation(shader, "model");
 }
 
 int main()
@@ -201,7 +205,10 @@ int main()
 
         glUseProgram(shader); // Bind our shader program
         
-            glUniform1f(uniformXMove, triOffset); // Updates the uniform with id "uniformXMove" with the value "triOffset"
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+
+            glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model)); // Updates the uniform with id "uniformModel" with the value "model"
 
             glBindVertexArray(VAO); // Bind the VAO we want OpenGL to render
             
